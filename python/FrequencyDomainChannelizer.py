306944c36ada39bab0e675d887212c391bd5fbd5
Gereon Such <gereonsuch@gmail.com>
2018-05-08 09:21:52 +0200

    Further logging information for visualization

--- python/FrequencyDomainChannelizer.py
+++ python/FrequencyDomainChannelizer.py
@@ -232,7 +232,10 @@ class FrequencyDomainChannelizer(gr.hier_block2):
             
             
         
-        
+        if self.verbose:
+            self.abssqr=blocks.complex_to_mag_squared(self.blocksize)
+            self.debugsink=blocks.file_sink(gr.sizeof_gr_complex*self.blocksize, 'gr-FDC.FreqDomChan.debugOutput.c64'.format(self.blocksize), False)
+            self.debugsink.set_unbuffered(False)
         
         
         
@@ -259,6 +262,10 @@ class FrequencyDomainChannelizer(gr.hier_block2):
             self.connect( (self.fft, 0), (self.activity_detection_channelizer, 0) )
             if msgoutput:
                 self.msg_connect( self.activity_detection_channelizer, pmt.intern("msgout"), self, pmt.intern(self.msgport) )
+        
+        if self.verbose:
+            self.connect( (self.fft, 0), (self.abssqr, 0) )
+            self.connect( (self.abssqr, 0), (self.debugsink, 0) )
         
         
     
